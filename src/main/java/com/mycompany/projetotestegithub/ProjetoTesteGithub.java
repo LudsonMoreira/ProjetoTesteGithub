7a836182b13458f019d0f81ac7ebe16e41085fca
LudsonMoreira <ludson.moreira@escola.pr.gov.br>
2026-09-15 12:20:51 -0300

    Criação da Classe Aluno

--- src/main/java/com/mycompany/projetotestegithub/ProjetoTesteGithub.java
+++ src/main/java/com/mycompany/projetotestegithub/ProjetoTesteGithub.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.projetotestegithub;
+import com.mycompany.usuarios.Aluno;
 
-/**
- *
- * @author pedagogico
- */
 public class ProjetoTesteGithub {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Aluno Ludson = new Aluno("Ludson Moreira", 25);
+        System.out.printf("%s", Ludson.apresentar());
     }
 }
